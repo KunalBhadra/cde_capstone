91a101f86d3e59781c1902a074649fc7421eda08
KB <kunal.bhadra@gmail.com>
2022-07-07 14:23:00 +0530

    commit

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Falsk(__name__)
 def homepage():
     return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project HomePage!</h1>"
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port =80, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port =80, debug=True)
