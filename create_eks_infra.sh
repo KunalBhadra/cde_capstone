7145a184d5e4fafb84bf76eb24decf3526ed3f86
KB <kunal.bhadra@gmail.com>
2022-07-14 15:36:22 +0530

    commit

--- create_eks_infra.sh
+++ create_eks_infra.sh
@@ -4,5 +4,5 @@ if [[ $count -gt 0 ]]
     echo "Skipping Cluster creation .. Infrastructure Exists"
     else
     echo "Creatiing AWS Kubernetes cluster"
-    eksctl create cluster --name $ENVIRONMENT_NAME- --version 1.19 --region $AWS_DEFAULT_REGION --nodegroup-name $ENVIRONMENT_NAME --node-type t3.large --nodes-min 2 --nodes-max 4 --managed
+    eksctl create cluster --name $ENVIRONMENT_NAME --version 1.19 --region $AWS_DEFAULT_REGION --nodegroup-name $ENVIRONMENT_NAME --node-type t3.large --nodes-min 2 --nodes-max 4 --managed
 fi
