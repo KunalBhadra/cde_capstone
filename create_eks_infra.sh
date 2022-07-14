c2743ed6ba8880a36eeaf9b4ba188b71c1197c4e
KB <kunal.bhadra@gmail.com>
2022-07-14 21:05:24 +0530

    commit

--- create_eks_infra.sh
+++
@@ -1,8 +0,0 @@
-count=$(grep -i eksctl-capstone-pickcolor-eks output.txt | wc -l)
-if [[ $count -gt 0 ]]
-    then 
-    echo "Skipping Cluster creation .. Infrastructure Exists"
-    else
-    echo "Creatiing AWS Kubernetes cluster"
-    eksctl create cluster --name $ENVIRONMENT_NAME-eks --version 1.19 --region $AWS_DEFAULT_REGION --nodegroup-name $ENVIRONMENT_NAME-node --node-type t3.large --nodes-min 2 --nodes-max 4 --managed
-fi
